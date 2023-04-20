1991d325d22213916b8332b606c44401b72d9ea0
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-04-19 20:03:58 -0400

    task1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+let myInterval = null;
 
+refs.btnStop.disabled = true;
+
+const btnStopDisabled = () => {
+  refs.btnStop.disabled = true;
+  refs.btnStart.disabled = false;
+};
+
+const btnStartDisabled = () => {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+};
+
+const onStart = () => {
+  myInterval = setInterval(colorForBackground, 1000);
+  btnStartDisabled();
+};
+const onStop = () => {
+  clearInterval(myInterval);
+  btnStopDisabled();
+};
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const colorForBackground = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+refs.btnStart.addEventListener('click', onStart);
+
+refs.btnStop.addEventListener('click', onStop);
