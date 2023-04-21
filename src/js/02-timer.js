2d3d44d2c04bf23e3539738726a18b3083b6f1f0
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-04-21 01:30:23 -0400

    task2 UI is being updated

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,7 +8,15 @@ import 'flatpickr/dist/flatpickr.min.css';
 const refs = {
   inputEl: document.querySelector("input[type = 'text']"),
   btnStart: document.querySelector('[data-start]'),
+  dataDays: document.querySelector('[data-days]'),
+  dataHours: document.querySelector('[data-hours]'),
+  dataMinutes: document.querySelector('[data-minutes]'),
+  dataSeconds: document.querySelector('[data-seconds]'),
 };
+let days = null;
+let hours = null;
+let minutes = null;
+let seconds = null;
 
 const options = {
   enableTime: true,
@@ -21,10 +29,45 @@ const options = {
     const currentTime = parseInt(options.defaultDate.getTime());
     if (selectedTime <= currentTime) {
       refs.btnStart.disabled = true;
+
+      updateUI(0, 0, 0, 0);
       return alert('Please choose a date in the future');
     }
+    const difference = selectedTime - currentTime;
+    convertMs(difference);
+
+    updateUI(days, hours, minutes, seconds);
     refs.btnStart.disabled = false;
   },
 };
 
 flatpickr(refs.inputEl, options);
+
+function updateUI(d, h, m, s) {
+  refs.dataDays.innerHTML = addLeadingZero(d);
+  refs.dataHours.innerHTML = addLeadingZero(h);
+  refs.dataMinutes.innerHTML = addLeadingZero(m);
+  refs.dataSeconds.innerHTML = addLeadingZero(s);
+}
+
+function addLeadingZero(value) {
+  return (result = value.toString().padStart(2, '0'));
+}
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  days = Math.floor(ms / day);
+  // Remaining hours
+  hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
